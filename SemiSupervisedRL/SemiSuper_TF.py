a676699ca3bcd4369481845f540dec8d67f0f825
Ashwini Venkatesh <asvenk@TACCs-MacBook-Pro.local>
2016-11-27 20:35:44 -0600

    scalar err

--- SemiSupervisedRL/SemiSuper_TF.py
+++ SemiSupervisedRL/SemiSuper_TF.py
@@ -216,7 +216,7 @@ def variable_summaries(var):
     """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
     with tf.name_scope('summaries'):
         mean = tf.reduce_mean(var)
-        tf.summary.scalar('mean', mean)
+        tf.summary_scalar('mean', mean)
     with tf.name_scope('stddev'):
         stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
         tf.scalar_summary('stddev', stddev)
